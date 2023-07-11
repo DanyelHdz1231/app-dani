5d9a7ed1d367b3ce3dc716d7fae2a6cc1dbebde6
DanyelHdz1231 <dany1231hdz@gmail.com>
2023-07-11 08:36:28 -0600

    Cambios 11/07

--- src/services/firebase/auth.js
+++ src/services/firebase/auth.js
@@ -1,36 +1,47 @@
-import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
-import { initialize } from "next/dist/server/lib/render-server";
-import app from ".";
-
-const auth = getAuth();
+import app from "./"
+import database from "./database"
 
 function login(email, password) {
-  signInWithEmailAndPassword(auth, email, password)
+
+    const auth = getAuth(app);
+
+    signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      // ...
+        // Signed in 
+        const user = userCredential.user;
+        console.log(userCredential)
+        // ...
     })
     .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage)
+    });
+
+}
+
+function singup(params){
+
+  const {email, password, displayname}= params;
+    const auth = getAuth(app);
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        database.collection("profiles").doc(user.uid).set({
+          email:user.email,
+          uid:user.uid
+          displayname,
+        })
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage)
+        // ..
     });
 }
 
-function register(email, password) {
-  const auth = getAuth(app);
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-    });
-}
-
-export { login, register }
+export { login, singup }
