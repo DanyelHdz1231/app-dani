6452bf6cd38d529d93f68bbd6812b8883df182b4
DanyelHdz1231 <dany1231hdz@gmail.com>
2023-07-11 08:36:51 -0600

    Cambios

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,9 +1,9 @@
 'use client'
 import React from "react"
-import { login } from "../../services/firebase/auth"
-import "./login.css"
+import { singup } from "../../services/firebase/auth"
+import "./register.css"
 
-function Login() {
+function Register() {
 
     const [state, setState] = React.useState({
         email: "",
@@ -20,18 +20,23 @@ function Login() {
         })
     }
 
-    function onLogin() {
+    function onRegister() {
         const {email, password} = state;
-        login(email, password)
+        const sendable={
+            email,
+            password,
+        }
+        singup(sendable)
     }
 
     return (
         <div className="container">
             <input onChange={onChange} name="email" type="text" value={state.email} />
             <input onChange={onChange} name="password" type="password" value={state.password} />
-            <button onClick={onLogin}>Login</button>
+            <input onChange={onChange} name="displayName" type="text" value={state.displayName}/>
+            <button onClick={onRegister}>Registrarse</button>
         </div>
     )
 }
 
-export default Login;
+export default Register;
